dca4e60841b0c6eff0d382b46b0b787d1181b311
kohmab <kohmab@gmail.com>
2024-04-19 15:37:14 +0300

    add accounts table

---
+++ src/main/webapp/html/loadPlayersList.js
@@ -0,0 +1,50 @@
+const fieldsColumnsMap = {
+    id: '#',
+    name: 'Name',
+    title: 'Title',
+    race: 'Race',
+    profession: 'Profession',
+    level: "Level",
+    birthday: "Birthday",
+    banned: "Banned"
+}
+
+function generateHead(){
+    let head = ''
+    for (let key in fieldsColumnsMap) {
+        head += '<th scope="col">'+ fieldsColumnsMap[key] +'</th>'
+    }
+    $("#accountListHead").html(head)
+}
+
+function format(key,string){
+    if (key === "birthday")
+        return new Date(string).toLocaleDateString()
+    return string
+}
+
+function generateRow(player){
+    let row = '<tr>'
+    let isFirstColumn = true
+    for (let key in fieldsColumnsMap){
+        if (isFirstColumn) {
+            row += '<th scope="row">' + format(key,player[key]) + '</th>'
+            isFirstColumn = false
+        } else {
+            row += '<td>' + format(key,player[key]) + '</td>'
+        }
+    }
+    row += '</tr>'
+    $('#accountsListTbody').append(row)
+}
+
+
+$.ajax({
+    url: '/rest/players',
+    dataType: 'json'
+}).done(function (players) {
+    generateHead()
+    for (const player of players) {
+        generateRow(player)
+    }
+})
